b81477e72a7d8f2aa740305953f47f419c619ec6
Grace Thompson <grace.thompson97@gmail.com>
2023-04-11 18:33:16 -0700

    finish prototype

--- bot.py
+++ bot.py
@@ -9,8 +9,15 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") #Same for OpenAI
 
 intents = discord.Intents.all() #needed for next line
 client = discord.Client(intents=intents) #as of api v2.0 this is required
-tree = app_commands.CommandTree(client) #to store all commands
+tree = app_commands.CommandTree(client) #to hold all commands
 
 @tree.command(name = "commandname", description = "My first application Command", guild=discord.Object(id=957119803397644378)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
 async def first_command(interaction):
     await interaction.response.send_message("Hello!")
+
+@client.event
+async def on_ready():
+    await tree.sync(guild=discord.Object(id=957119803397644378))
+    print("Ready!") #sync once ready
+
+client.run(DISCORD_TOKEN)
