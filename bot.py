0e12f19449b6d22ec8bf7d0fb3b3280c5effd8d7
Grace Thompson <grace.thompson97@gmail.com>
2023-04-11 18:44:56 -0700

    add openai

--- bot.py
+++ bot.py
@@ -6,11 +6,18 @@ import openai
 
 DISCORD_TOKEN = os.getenv("SMARTBOT_TOKEN") #Client secret for Discord bot, set as an env variable
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") #Same for OpenAI
+GUILD_ID = os.getenv("GUILD_ID")
+# OPENAI_ORG = os.getenv("OPENAI_ORG") unneeded unless you belong to multiple orgs
 
 intents = discord.Intents.all() #needed for next line
 client = discord.Client(intents=intents) #as of api v2.0 this is required
 tree = app_commands.CommandTree(client) #to hold all commands
 
+openai.organization = "org-Rsl5JS63ShHy62MuJZS2RKtL"
+openai.api_key = os.getenv(OPENAI_API_KEY)
+openai.Model.list()
+
+'''
 @tree.command(name = "commandname", description = "My first application Command", guild=discord.Object(id=957119803397644378)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
 async def first_command(interaction):
     await interaction.response.send_message("Hello!")
@@ -21,3 +28,4 @@ async def on_ready():
     print("Ready!") #sync once ready
 
 client.run(DISCORD_TOKEN)
+'''
